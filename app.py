273778be9c8b021e7a4e671460c9998a9e32ed38
Мацькевич Олександр <ofreg13@gmail.com>
2025-05-15 15:37:33 +0300

    lab9

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from api.views import books_router
-from api.auth import auth_router
-
-app = FastAPI()
-
-app.include_router(auth_router, prefix="/api/v1/auth")
-
-app.include_router(books_router, prefix="/api/v1/books")
